362358aab6108baaa131d5dcca9d047f55395a26
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-09-16 00:02:12 -0700

    Finished Q4

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,12 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    ints = a.map { |s| s.to_i }
+    plus2 = ints.map { |i| i + 2 }
+    even = plus2.keep_if { |i| i.even? }
+    unique = even.uniq
+    lessthan10 = unique.reject { |i| i >= 10 }
+    return lessthan10.inject(0, &:+)
   end
+
 end
